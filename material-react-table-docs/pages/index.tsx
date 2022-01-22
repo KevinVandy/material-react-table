92eb2ba3bfd7bf2f2b8035111e2a435ed4ee2adf
Kevin Vandy <kevinvandy656@gmail.com>
2022-01-22 15:40:28 -0600

    create docs project

---
+++ material-react-table-docs/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Material React Table</title>
+        <meta name="description" content="Material React Table Docs" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
