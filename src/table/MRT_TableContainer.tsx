3a34601a16d1f9980f78fd8d6ec324503a33f658
Kevin Vandy <kevinvandy656@gmail.com>
2022-02-15 21:11:04 -0600

    fix full screen issue

--- src/table/MRT_TableContainer.tsx
+++ src/table/MRT_TableContainer.tsx
@@ -24,7 +24,7 @@ const TableContainer = styled(MuiTableContainer, {
   top: fullScreen ? '0' : undefined,
   transition: 'all 0.2s ease-in-out',
   width: fullScreen ? '100vw' : undefined,
-  zIndex: 1200,
+  zIndex: fullScreen ? 1200 : undefined,
 }));
 
 const CircularProgressWrapper = styled('div')(({ theme }) => ({
