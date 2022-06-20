26301c04d128d38f5c0cb6ea6d4fb362c42c0c58
Kevin Vandy <kevinvandy656@gmail.com>
2022-06-19 20:38:43 -0600

    move minimal example to codesandbox

--- material-react-table-docs/examples/minimal/index.tsx
+++ material-react-table-docs/examples/minimal/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { SourceCodeSnippet } from '../../components/mdx/SourceCodeSnippet';
-import Example from './TS';
-const JS = require('!!raw-loader!./JS.js').default;
-const TS = require('!!raw-loader!./TS.tsx').default;
+import Example from './sandbox/src/TS';
+const JS = require('!!raw-loader!./sandbox/src/JS.js').default;
+const TS = require('!!raw-loader!./sandbox/src/TS.tsx').default;
 
 const ExampleTable = () => {
   return (
     <SourceCodeSnippet
+      codeSandboxURL="https://codesandbox.io/s/github/KevinVandy/material-react-table/tree/main/material-react-table-docs/examples/minimal/sandbox?file=/src/TS.tsx"
       Component={Example}
       javaScriptCode={JS}
       typeScriptCode={TS}
