d2f0bdef065081647d5282b39e7f9cd6ef2dacc2
Kevin Vandy <kevinvandy656@gmail.com>
2022-06-30 23:47:11 -0500

    remove unnecessary configs?

--- material-react-table-docs/next.config.js
+++ material-react-table-docs/next.config.js
@@ -14,24 +14,4 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
   ...nextConfig,
   pageExtensions: ['js', 'ts', 'tsx', 'jsx', 'md', 'mdx'],
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ];
-  },
 });
