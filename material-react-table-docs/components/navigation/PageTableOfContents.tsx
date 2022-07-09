2fefd89b173d849b5f73dc28e9b5c2f497e4cf77
Kevin Vandy <kevinvandy656@gmail.com>
2022-07-09 16:11:10 -0500

    add mini table of contents to each docs page

---
+++ material-react-table-docs/components/navigation/PageTableOfContents.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import { Box, Link, Typography, useMediaQuery } from '@mui/material';
+import { useRouter } from 'next/router';
+
+const PageTableOfContents = () => {
+  const { pathname } = useRouter();
+  const isXLDesktop = useMediaQuery('(min-width: 1800px)');
+  const [headings, setHeadings] = useState<NodeListOf<HTMLElement>>();
+
+  useEffect(() => {
+    setHeadings(
+      document.querySelectorAll(isXLDesktop ? 'h2, h3, h4, h5' : 'h3, h4, h5'),
+    );
+  }, [isXLDesktop, pathname]);
+
+  if (pathname === '/') return null;
+  if (pathname === '/about') return null;
+
+  return (
+    <Box
+      sx={{
+        position: isXLDesktop ? 'fixed' : undefined,
+        top: '80px',
+        right: '2rem',
+        minWidth: '100px',
+        maxWidth: isXLDesktop ? '250px' : '500px',
+      }}
+    >
+      <ul style={{ padding: 0 }}>
+        {Array.from(headings ?? []).map((heading, index) => {
+          console.log(heading.innerText);
+          if (
+            !isXLDesktop &&
+            ['demo', 'source code'].includes(
+              heading.innerText.toLowerCase().trim(),
+            )
+          ) {
+            return;
+          }
+          return (
+            <li
+              key={index}
+              style={{
+                listStyle: 'none',
+                paddingLeft:
+                  heading.localName === 'h3'
+                    ? '1rem'
+                    : heading.localName === 'h4'
+                    ? '2rem'
+                    : heading.localName === 'h5'
+                    ? '3rem'
+                    : 0,
+              }}
+            >
+              <Link
+                href={`#${heading.id}`}
+                sx={(theme) => ({
+                  color:
+                    theme.palette.grey[
+                      theme.palette.mode === 'dark' ? 400 : 700
+                    ],
+                })}
+              >
+                <Typography component="span" variant="subtitle2">
+                  {heading.innerText}
+                </Typography>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </Box>
+  );
+};
+
+export default PageTableOfContents;
--- material-react-table-docs/pages/_app.tsx
+++ material-react-table-docs/pages/_app.tsx
@@ -2,30 +2,19 @@ import { useState, useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { MDXProvider } from '@mdx-js/react';
-import { styled, ThemeProvider, useMediaQuery } from '@mui/material';
+import { Box, styled, ThemeProvider, useMediaQuery } from '@mui/material';
 import { mdxComponents } from '../components/mdx/mdxComponents';
 import TopBar from '../components/navigation/TopBar';
 import SideBar from '../components/navigation/Sidebar';
 import BreadCrumbs from '../components/navigation/BreadCrumbs';
 import { theme } from '../styles/MuiTheme';
 import '../styles/globals.css';
-
-const PageContainer = styled('div')(({ theme }) => ({
-  backgroundColor: theme.palette.background.default,
-  color: theme.palette.text.primary,
-  minHeight: '100vh',
-  transition: 'all 200ms ease-in-out',
-}));
-
-const PageContent = styled('div')({
-  maxWidth: '1200px',
-  margin: 'auto',
-  transition: 'all 200ms ease-in-out',
-});
+import PageTableOfContents from '../components/navigation/PageTableOfContents';
 
 function App({ Component, pageProps }: AppProps) {
   const isTablet = useMediaQuery('(max-width: 900px)');
   const isDesktop = useMediaQuery('(min-width: 1600px)');
+  const isXLDesktop = useMediaQuery('(min-width: 1800px)');
   const [navOpen, setNavOpen] = useState(true);
   const [darkTheme, setDarkTheme] = useState(true);
 
@@ -45,7 +34,7 @@ function App({ Component, pageProps }: AppProps) {
         <title>Material React Table</title>
         <meta
           name="description"
-          content="Material React Table Docs. A fully featured Material UI implementation of TanStack Table v8. Written from the ground up in TypeScript."
+          content="Material React Table, a fully featured Material UI implementation of TanStack React Table V8. Written from the ground up in TypeScript."
         />
         <link rel="icon" href="/mrt_logo.png" />
       </Head>
@@ -58,18 +47,31 @@ function App({ Component, pageProps }: AppProps) {
             setNavOpen={setNavOpen}
           />
           <SideBar navOpen={navOpen || isDesktop} setNavOpen={setNavOpen} />
-          <PageContainer
-            sx={{
-              p: `64px 32px 800px ${
+          <Box
+            sx={(theme) => ({
+              backgroundColor: theme.palette.background.default,
+              color: theme.palette.text.primary,
+              minHeight: '100vh',
+              p: `64px ${isXLDesktop ? '250px' : '32px'} 800px ${
                 (navOpen || isDesktop) && !isTablet ? '300px' : '32px'
               }`,
-            }}
+              transition: 'all 200ms ease-in-out',
+            })}
           >
-            <PageContent>
+            <Box
+              sx={{
+                maxWidth: '1200px',
+                margin: 'auto',
+                transition: 'all 200ms ease-in-out',
+                width: '100%',
+              }}
+            >
               <BreadCrumbs />
+              {!isXLDesktop && <PageTableOfContents />}
               <Component {...pageProps} />
-            </PageContent>
-          </PageContainer>
+            </Box>
+            {isXLDesktop && <PageTableOfContents />}
+          </Box>
         </MDXProvider>
       </ThemeProvider>
     </>
