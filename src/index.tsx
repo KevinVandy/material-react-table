8853cb24c49604979f38a4bada44c4113c8c11d0
Kevin Vandy <kevinvandy656@gmail.com>
2022-07-31 21:57:53 -0500

    switch from tsdx to custom rollup

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,3 @@
 import MaterialReactTable from './MaterialReactTable';
 export default MaterialReactTable;
 export * from './MaterialReactTable';
-
-import type { MRT_Icons } from './icons';
-export type { MRT_Icons };
-
-import type { MRT_Localization } from './localization';
-export type { MRT_Localization };
