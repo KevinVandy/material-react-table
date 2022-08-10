ab1422f392075c5d5db190ba43243db38ed8fc7a
Kevin Vandy <kevinvandy656@gmail.com>
2022-08-10 00:01:59 -0500

    add click to copy docs

---
+++ material-react-table-docs/examples/enable-click-to-copy/sandbox/src/TS.tsx
@@ -0,0 +1,39 @@
+import React, { FC, useMemo } from 'react';
+import MaterialReactTable, { MRT_ColumnDef } from 'material-react-table';
+import { ContentCopy } from '@mui/icons-material';
+import { data, Person } from './makeData';
+
+const Example: FC = () => {
+  const columns = useMemo<MRT_ColumnDef<Person>[]>(
+    () => [
+      {
+        accessorKey: 'firstName',
+        header: 'First Name',
+      },
+      {
+        accessorKey: 'lastName',
+        header: 'Last Name',
+      },
+      {
+        accessorKey: 'email',
+        header: 'Email',
+        enableClickToCopy: true,
+        muiTableBodyCellCopyButtonProps: {
+          fullWidth: true,
+          startIcon: <ContentCopy />,
+          sx: { justifyContent: 'flex-start' },
+        },
+      },
+      {
+        accessorKey: 'city',
+        header: 'City',
+        enableClickToCopy: true,
+      },
+    ],
+    [],
+  );
+
+  return <MaterialReactTable columns={columns} data={data} />;
+};
+
+export default Example;
---
+++ material-react-table-docs/examples/enable-click-to-copy/sandbox/src/makeData.ts
@@ -0,0 +1,39 @@
+export type Person = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  city: string;
+};
+
+export const data: Person[] = [
+  {
+    firstName: 'Dylan',
+    lastName: 'Murray',
+    email: 'dmurray@yopmail.com',
+    city: 'East Daphne',
+  },
+  {
+    firstName: 'Raquel',
+    lastName: 'Kohler',
+    email: 'rkholer33@yopmail.com',
+    city: 'Columbus',
+  },
+  {
+    firstName: 'Ervin',
+    lastName: 'Reinger',
+    email: '566 Brakus Inlet',
+    city: 'South Linda',
+  },
+  {
+    firstName: 'Brittany',
+    lastName: 'McCullough',
+    email: 'bmccullough44@mailinator.com',
+    city: 'Lincoln',
+  },
+  {
+    firstName: 'Branson',
+    lastName: 'Frami',
+    email: 'bframi@yopmain.com',
+    city: 'New York',
+  },
+];
