4cb996ff72499ad68a002eb9fd96ca7c71b94397
Kevin Vandy <kevinvandy656@gmail.com>
2022-09-08 18:50:24 -0500

    add docs for table instance apis

---
+++ material-react-table-docs/components/prop-tables/TableInstanceAPIsTable.tsx
@@ -0,0 +1,134 @@
+import React, { FC, useEffect, useMemo, useState } from 'react';
+import Link from 'next/link';
+import MaterialReactTable, {
+  MRT_ColumnDef,
+  MRT_TableInstance,
+} from 'material-react-table';
+import { Link as MuiLink, Typography, useMediaQuery } from '@mui/material';
+import { TableInstanceAPI, tableInstanceAPIs } from './tableInstanceAPIs';
+
+interface Props {
+  onlyProps?: Set<keyof MRT_TableInstance>;
+}
+
+const TableInstanceAPIsTable: FC<Props> = ({ onlyProps }) => {
+  const isDesktop = useMediaQuery('(min-width: 1200px)');
+
+  const columns = useMemo(
+    () =>
+      [
+        {
+          accessorKey: 'tableInstanceAPI',
+          enableClickToCopy: true,
+          header: 'State Option',
+          muiTableBodyCellCopyButtonProps: ({ cell }) => ({
+            className: 'table-instance-api',
+            id: `${cell.getValue<string>()}-table-instance-api`,
+          }),
+          Cell: ({ cell }) => cell.getValue<string>(),
+        },
+        {
+          accessorKey: 'description',
+          enableGlobalFilter: false,
+          header: 'Description',
+        },
+        {
+          accessorKey: 'link',
+          disableFilters: true,
+          enableGlobalFilter: false,
+          header: 'More Info Links',
+          Cell: ({ cell, row }) => (
+            <Link href={cell.getValue() as string} passHref>
+              <MuiLink
+                target={
+                  (cell.getValue() as string).startsWith('http')
+                    ? '_blank'
+                    : undefined
+                }
+                rel="noreferrer"
+              >
+                {row.original?.linkText}
+              </MuiLink>
+            </Link>
+          ),
+        },
+      ] as MRT_ColumnDef<TableInstanceAPI>[],
+    [],
+  );
+
+  const [columnPinning, setColumnPinning] = useState({});
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (isDesktop) {
+        setColumnPinning({
+          left: ['mrt-row-expand', 'mrt-row-numbers', 'tableInstanceAPI'],
+          right: ['link'],
+        });
+      } else {
+        setColumnPinning({});
+      }
+    }
+  }, [isDesktop]);
+
+  const data = useMemo(() => {
+    if (onlyProps) {
+      return tableInstanceAPIs.filter(({ tableInstanceAPI }) =>
+        onlyProps.has(tableInstanceAPI),
+      );
+    }
+    return tableInstanceAPIs;
+  }, [onlyProps]);
+
+  return (
+    <MaterialReactTable
+      columns={columns}
+      data={data}
+      displayColumnDefOptions={{
+        'mrt-row-numbers': {
+          size: 10,
+        },
+        'mrt-row-expand': {
+          size: 10,
+        },
+      }}
+      enableColumnActions={!onlyProps}
+      enableColumnFilterModes
+      enableColumnOrdering={!onlyProps}
+      enablePagination={false}
+      enablePinning
+      enableRowNumbers
+      enableBottomToolbar={false}
+      enableTopToolbar={!onlyProps}
+      initialState={{
+        columnVisibility: { description: false },
+        density: 'compact',
+        showGlobalFilter: true,
+        sorting: [{ id: 'tableInstanceAPI', desc: false }],
+      }}
+      muiSearchTextFieldProps={{
+        placeholder: 'Search Table APIs',
+        sx: { minWidth: '18rem' },
+        variant: 'outlined',
+      }}
+      muiTablePaperProps={{
+        sx: { mb: '1.5rem' },
+        id: onlyProps
+          ? 'relevant-table-instance-apis-table'
+          : 'table-instance-apis-table',
+      }}
+      positionGlobalFilter="left"
+      renderDetailPanel={({ row }) => (
+        <Typography
+          color={row.original.description ? 'secondary.main' : 'text.secondary'}
+        >
+          {row.original.description || 'No Description Provided... Yet...'}
+        </Typography>
+      )}
+      rowNumberMode="static"
+      state={{ columnPinning }}
+    />
+  );
+};
+
+export default TableInstanceAPIsTable;
