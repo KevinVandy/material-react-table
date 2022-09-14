9e0f83b84cc709cbe547999883ace422e9ba5cb1
Kevin Vandy <kevinvandy656@gmail.com>
2022-09-13 22:49:54 -0500

    remove locale from basic example

--- material-react-table-docs/examples/basic/sandbox/src/TS.tsx
+++ material-react-table-docs/examples/basic/sandbox/src/TS.tsx
@@ -1,6 +1,5 @@
 import React, { FC, useMemo } from 'react';
 import MaterialReactTable, { MRT_ColumnDef } from 'material-react-table';
-import { MRT_Localization_PT_BR } from 'material-react-table/locales/pt-BR';
 
 type Person = {
   name: {
@@ -89,13 +88,7 @@ const Example: FC = () => {
     [],
   );
 
-  return (
-    <MaterialReactTable
-      columns={columns}
-      data={data}
-      localization={MRT_Localization_PT_BR}
-    />
-  );
+  return <MaterialReactTable columns={columns} data={data} />;
 };
 
 export default Example;
