53a85280caf31cf9278927ea0f74d6fcf6049dda
Kevin Vandy <kevinvandy656@gmail.com>
2022-09-17 13:21:45 -0500

    fix sticky header with pinned columns in full-screen mode

--- src/head/MRT_TableHead.tsx
+++ src/head/MRT_TableHead.tsx
@@ -10,21 +10,26 @@ interface Props {
 export const MRT_TableHead: FC<Props> = ({ table }) => {
   const {
     getHeaderGroups,
-    options: { enableStickyHeader, muiTableHeadProps },
+    getState,
+    options: { enableStickyHeader, muiTableHeadProps, enableRowVirtualization },
   } = table;
+  const { isFullScreen } = getState();
 
   const tableHeadProps =
     muiTableHeadProps instanceof Function
       ? muiTableHeadProps({ table })
       : muiTableHeadProps;
 
+  const stickyHeader =
+    enableStickyHeader || enableRowVirtualization || isFullScreen;
+
   return (
     <TableHead
       {...tableHeadProps}
       sx={(theme) => ({
         opacity: 0.97,
-        position: enableStickyHeader ? 'sticky' : undefined,
-        zIndex: enableStickyHeader ? 2 : undefined,
+        position: stickyHeader ? 'sticky' : undefined,
+        zIndex: stickyHeader ? 2 : undefined,
         ...(tableHeadProps?.sx instanceof Function
           ? tableHeadProps?.sx(theme)
           : (tableHeadProps?.sx as any)),
