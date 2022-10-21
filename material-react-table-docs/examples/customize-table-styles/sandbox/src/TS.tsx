48a18a3334bee7ec853324bf759b6d3dfdf0aa65
Kevin Vandy <kevinvandy656@gmail.com>
2022-10-20 19:39:42 -0500

    add some striped row docs

---
+++ material-react-table-docs/examples/customize-table-styles/sandbox/src/TS.tsx
@@ -0,0 +1,68 @@
+import React, { FC, useMemo } from 'react';
+import MaterialReactTable, { MRT_ColumnDef } from 'material-react-table';
+import { data, Person } from './makeData';
+import { darken } from '@mui/material';
+
+const Example: FC = () => {
+  const columns = useMemo<MRT_ColumnDef<Person>[]>(
+    //column definitions...
+    () => [
+      {
+        accessorKey: 'id',
+        header: 'ID',
+        size: 50,
+      },
+      {
+        accessorKey: 'firstName',
+        header: 'First Name',
+      },
+      {
+        accessorKey: 'middleName',
+        header: 'Middle Name',
+      },
+      {
+        accessorKey: 'lastName',
+        header: 'Last Name',
+      },
+      {
+        accessorKey: 'address',
+        header: 'Address',
+        size: 300,
+      },
+      {
+        accessorKey: 'city',
+        header: 'City',
+      },
+
+      {
+        accessorKey: 'state',
+        header: 'State',
+      },
+    ],
+    [],
+    //end
+  );
+
+  return (
+    <MaterialReactTable
+      columns={columns}
+      data={data}
+      muiTablePaperProps={{
+        elevation: 0,
+        sx: {
+          borderRadius: '0',
+          border: '1px dashed #e0e0e0',
+        },
+      }}
+      muiTableBodyProps={{
+        sx: (theme) => ({
+          '& tr:nth-of-type(odd)': {
+            backgroundColor: darken(theme.palette.background.default, 0.1),
+          },
+        }),
+      }}
+    />
+  );
+};
+
+export default Example;
---
+++ material-react-table-docs/examples/customize-table-styles/sandbox/src/makeData.ts
@@ -0,0 +1,57 @@
+export type Person = {
+  id: string;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  address: string;
+  city: string;
+  state: string;
+};
+
+export const data = [
+  {
+    id: '1',
+    firstName: 'Dylan',
+    middleName: 'Sprouse',
+    lastName: 'Murray',
+    address: '261 Erdman Ford',
+    city: 'East Daphne',
+    state: 'Kentucky',
+  },
+  {
+    id: '2',
+    firstName: 'Raquel',
+    middleName: 'Hakeem',
+    lastName: 'Kohler',
+    address: '769 Dominic Grove',
+    city: 'Columbus',
+    state: 'Ohio',
+  },
+  {
+    id: '3',
+    firstName: 'Ervin',
+    middleName: 'Kris',
+    lastName: 'Reinger',
+    address: '566 Brakus Inlet',
+    city: 'South Linda',
+    state: 'West Virginia',
+  },
+  {
+    id: '4',
+    firstName: 'Brittany',
+    middleName: 'Kathryn',
+    lastName: 'McCullough',
+    address: '722 Emie Stream',
+    city: 'Lincoln',
+    state: 'Nebraska',
+  },
+  {
+    id: '5',
+    firstName: 'Branson',
+    middleName: 'John',
+    lastName: 'Frami',
+    address: '32188 Larkin Turnpike',
+    city: 'Charleston',
+    state: 'South Carolina',
+  },
+];
