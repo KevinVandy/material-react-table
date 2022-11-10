4d93528eac3423494cc538592296744451a190ec
Kevin Vandy <kevinvandy656@gmail.com>
2022-11-09 22:02:22 -0600

    next config change

--- material-react-table-docs/next-sitemap.config.cjs
+++ material-react-table-docs/next-sitemap.config.cjs
@@ -3,6 +3,26 @@ const config = {
   siteUrl: 'https://www.material-react-table.com',
   generateRobotsTxt: true,
   changefreq: 'monthly',
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
   robotsTxtOptions: {
     policies: [
       {
