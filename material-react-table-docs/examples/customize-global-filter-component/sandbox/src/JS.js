ff4538fd307045d0d75f2572bb0899b4294a4a06
Kevin Vandy <kevinvandy656@gmail.com>
2022-11-27 12:02:26 -0600

    add customized global filter example

---
+++ material-react-table-docs/examples/customize-global-filter-component/sandbox/src/JS.js
@@ -0,0 +1,52 @@
+import React, { useMemo } from 'react';
+import MaterialReactTable from 'material-react-table';
+import { data } from './makeData';
+
+const Example = () => {
+  const columns = useMemo(
+    //column definitions...
+    () => [
+      {
+        accessorKey: 'id',
+        header: 'ID',
+      },
+      {
+        accessorKey: 'firstName',
+        header: 'First Name',
+      },
+      {
+        accessorKey: 'middleName',
+        header: 'Middle Name',
+      },
+      {
+        accessorKey: 'lastName',
+        header: 'Last Name',
+      },
+      {
+        accessorKey: 'age',
+        header: 'Age',
+      },
+    ],
+    [],
+    //end
+  );
+
+  return (
+    <MaterialReactTable
+      columns={columns}
+      data={data}
+      enableGlobalFilterModes
+      initialState={{
+        showGlobalFilter: true,
+      }}
+      positionGlobalFilter="left"
+      muiSearchTextFieldProps={{
+        placeholder: `Search ${data.length} rows`,
+        sx: { minWidth: '300px' },
+        variant: 'outlined',
+      }}
+    />
+  );
+};
+
+export default Example;
