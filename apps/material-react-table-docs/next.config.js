7e82d42aad9f70c69cbae3c25ab4e455918a0948
Kevin Vandy <kevinvandy656@gmail.com>
2022-11-29 22:18:55 -0600

    package upgrades

--- apps/material-react-table-docs/next.config.js
+++ apps/material-react-table-docs/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  experimental: {
+    transpilePackages: ["material-react-table"],
+  },
   reactStrictMode: true,
   i18n: {
     locales: ['en'],
--- packages/material-react-table/index.ts
+++ packages/material-react-table/index.ts
@@ -1,1 +1,3 @@
+import MaterialReactTable from './src/MaterialReactTable';
+export default MaterialReactTable;
 export * from './src/index';
