6c2bf0cd747da60b276a89e512e895bbf83bef57
Kevin Vandy <kevinvandy656@gmail.com>
2023-04-15 20:24:28 -0500

    split rollup up for locales

--- packages/material-react-table/rollup.config.mjs
+++ packages/material-react-table/rollup.config.mjs
@@ -1,35 +1,8 @@
-import copy from 'rollup-plugin-copy';
 import dts from 'rollup-plugin-dts';
 import external from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import { babel } from '@rollup/plugin-babel';
-
-const supportedLocales = [
-  'cs',
-  'da',
-  'de',
-  'en',
-  'es',
-  'fa',
-  'fr',
-  'it',
-  'ja',
-  'nl',
-  'pl',
-  'pt',
-  'pt-BR',
-  'ro',
-  'ru',
-  'sr-Cyrl-RS',
-  'sr-Latn-RS',
-  'sv',
-  'tr',
-  'uk',
-  'vi',
-  'zh-Hans',
-  'zh-Hant',
-];
 
 export default [
   {
@@ -65,34 +38,6 @@ export default [
       typescript(),
     ],
   },
-  ...supportedLocales.map((locale) => ({
-    input: `./src/_locales/${locale}.ts`,
-    output: [
-      {
-        file: `./locales/${locale}.js`,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: `./locales/${locale}.esm.js`,
-        format: 'esm',
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      typescript({ declaration: false, declarationDir: undefined }),
-      copy({
-        targets: [
-          ...['cjs', 'esm'].map((format) => ({
-            src: `./dist/esm/types/_locales/${locale}.d.ts`,
-            dest: './locales',
-            rename: () =>
-              format === 'esm' ? `${locale}.${format}.d.ts` : `${locale}.d.ts`,
-          })),
-        ],
-      }),
-    ],
-  })),
   {
     input: './dist/esm/types/index.d.ts',
     output: [{ file: './dist/index.d.ts', format: 'esm' }],
