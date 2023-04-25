ef0c7542e60bfcece70eb2c297175d3d19adb68f
Joey <joeylau1998@gmail.com>
2023-04-25 22:13:07 +0200

    Clicking "Filter by" will open sub menu when column filter is open (#496)
    * open filter mode menu when pressing "filter by" while column filter is open
    
    * Added back the `disabled`

--- packages/material-react-table/src/menus/MRT_ColumnActionMenu.tsx
+++ packages/material-react-table/src/menus/MRT_ColumnActionMenu.tsx
@@ -232,7 +232,7 @@ export const MRT_ColumnActionMenu = ({
             disabled={showColumnFilters && !enableColumnFilterModes}
             divider={enableGrouping || enableHiding}
             key={1}
-            onClick={handleFilterByColumn}
+            onClick={getState().showColumnFilters ? handleOpenFilterModeMenu : handleFilterByColumn}
             sx={commonMenuItemStyles}
           >
             <Box sx={commonListItemStyles}>
