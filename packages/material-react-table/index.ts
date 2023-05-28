907ee1c9ea3a0e794f042e44615c91ae9e31d8c6
Kevin Vandy <kevinvandy656@gmail.com>
2023-05-28 12:31:18 -0500

    faker upgrade

--- packages/material-react-table/index.ts
+++ packages/material-react-table/index.ts
@@ -1,4 +1,7 @@
 // This just for docs real-time development when the `dist` folder is also deleted.
 import MaterialReactTable from './src';
+/**
+ * @deprecated Use `import { MaterialReactTable } from 'material-react-table';` instead
+ */
 export default MaterialReactTable;
 export * from './src';
