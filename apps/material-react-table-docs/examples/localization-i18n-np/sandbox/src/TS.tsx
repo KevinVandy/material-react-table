011420cd0f169c90cd4472f450de0775717d284b
Achyut Neupane <30431426+achyutkneupane@users.noreply.github.com>
2023-10-07 04:45:43 +0545

    Nepali Language Translations added (#715)
    * Nepali Language Translations added
    
    * Documentation for Nepali Table Created
    
    * Nepali Option added to Locale Example Table
    
    * Package Config updated
    
    * HTML lang updated
    
    * Table Header changed for example table
    
    * Build Locales updated

---
+++ apps/material-react-table-docs/examples/localization-i18n-np/sandbox/src/TS.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+//Import Material React Table and its Types
+import { MaterialReactTable, type MRT_ColumnDef } from 'material-react-table';
+
+//Import Material React Table Translations
+import { MRT_Localization_NP } from 'material-react-table/locales/np';
+
+//mock data
+import { data, type Person } from './makeData';
+
+const columns: MRT_ColumnDef<Person>[] = [
+  //column definitions...
+  {
+    accessorKey: 'firstName',
+    header: 'नाम'
+  },
+  {
+    accessorKey: 'lastName',
+    header: 'थर',
+    enableClickToCopy: true,
+  },
+  {
+    accessorKey: 'age',
+    header: 'उमेर',
+  },
+  //end
+];
+
+const Example = () => {
+  return (
+    <MaterialReactTable
+      columns={columns}
+      data={data}
+      enableColumnFilterModes
+      enableColumnOrdering
+      enableEditing
+      enableColumnPinning
+      enableRowActions
+      enableRowSelection
+      enableSelectAll={false}
+      initialState={{ showColumnFilters: true, showGlobalFilter: true }}
+      localization={MRT_Localization_NP}
+    />
+  );
+};
+
+//App.tsx or similar
+import { createTheme, ThemeProvider, useTheme } from '@mui/material';
+import { neNP } from '@mui/material/locale';
+
+const ExampleWithThemeProvider = () => {
+  const theme = useTheme(); //replace with your theme/createTheme
+  return (
+    //Setting Material UI locale as best practice to result in better accessibility
+    <ThemeProvider theme={createTheme(theme, neNP)}>
+      <Example />
+    </ThemeProvider>
+  );
+};
+
+export default ExampleWithThemeProvider;
---
+++ apps/material-react-table-docs/examples/localization-i18n-np/sandbox/src/makeData.ts
@@ -0,0 +1,23 @@
+export type Person = {
+  firstName: string;
+  lastName: string;
+  age: string;
+};
+
+export const data: Person[] = [
+  {
+    firstName: 'Kevin',
+    lastName: 'Vandy',
+    age: "२६",
+  },
+  {
+    firstName: 'Theodore',
+    lastName: 'Browne',
+    age: "२८",
+  },
+  {
+    firstName: 'Tanner',
+    lastName: 'Linsley',
+    age: "३३",
+  },
+];
