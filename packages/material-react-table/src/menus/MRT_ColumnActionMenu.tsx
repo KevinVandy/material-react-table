64dd48173624775262ba64a33dacb486afdbba44
Alex Gotardi <87832147+alexzrp@users.noreply.github.com>
2024-01-05 02:38:19 -0300

    Fixed Show clear filter menu if emptyModes filter fns is selected (#914)
    * Hide clear filter menu
    
    If a filter fns empty or notEmpty is used, the option clear filter in menu dont need show.
    The bug founded if select empty, because filter value is a space char
    
    * sort
    
    * fix clear menu item instead of remove
    
    ---------
    
    Co-authored-by: Kevin Van Cott <kevinvandy656@gmail.com>

--- packages/material-react-table/src/menus/MRT_ColumnActionMenu.tsx
+++ packages/material-react-table/src/menus/MRT_ColumnActionMenu.tsx
@@ -67,6 +67,7 @@ export const MRT_ColumnActionMenu = <TData extends MRT_RowData>({
       renderColumnActionsMenuItems,
     },
     refs: { filterInputRefs },
+    setColumnFilterFns,
     setColumnOrder,
     setColumnSizingInfo,
     setShowColumnFilters,
@@ -119,8 +120,14 @@ export const MRT_ColumnActionMenu = <TData extends MRT_RowData>({
   };
 
   const handleClearFilter = () => {
-    column.setFilterValue('');
+    column.setFilterValue(undefined);
     setAnchorEl(null);
+    if (['empty', 'notEmpty'].includes(columnDef._filterFn)) {
+      setColumnFilterFns((prev) => ({
+        ...prev,
+        [header.id]: allowedColumnFilterOptions?.[0] ?? 'fuzzy',
+      }));
+    }
   };
 
   const handleFilterByColumn = () => {
