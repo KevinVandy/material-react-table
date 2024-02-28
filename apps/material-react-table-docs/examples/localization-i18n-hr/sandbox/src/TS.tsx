de8dc86b9cda8a44b25720f0d4c6ec02b7adfcda
Renato <72624715+renatoka@users.noreply.github.com>
2024-02-28 05:18:43 +0100

    Add Croatian locale (#1011)
    * implemented croatian translations
    
    * additional locale implementations

---
+++ apps/material-react-table-docs/examples/localization-i18n-hr/sandbox/src/TS.tsx
@@ -0,0 +1,60 @@
+//Import Material React Table and its Types
+import { MaterialReactTable, type MRT_ColumnDef } from 'material-react-table';
+
+//Import Material React Table Translations
+import { MRT_Localization_HR } from 'material-react-table/locales/hr';
+
+//mock data
+import { data, type Person } from './makeData';
+
+const columns: MRT_ColumnDef<Person>[] = [
+  //column definitions...
+  {
+    accessorKey: 'firstName',
+    header: 'Ime',
+  },
+  {
+    accessorKey: 'lastName',
+    header: 'Prezime',
+    enableClickToCopy: true,
+  },
+  {
+    accessorKey: 'age',
+    header: 'Godine',
+  },
+  //end
+];
+
+const Example = () => {
+  return (
+    <MaterialReactTable
+      columns={columns}
+      data={data}
+      enableColumnFilterModes
+      enableColumnOrdering
+      enableEditing
+      enableColumnPinning
+      enableRowActions
+      enableRowSelection
+      enableSelectAll={false}
+      initialState={{ showColumnFilters: true, showGlobalFilter: true }}
+      localization={MRT_Localization_HR}
+    />
+  );
+};
+
+//App.tsx or similar
+import { createTheme, ThemeProvider, useTheme } from '@mui/material';
+import { hrHR } from '@mui/material/locale';
+
+const ExampleWithThemeProvider = () => {
+  const theme = useTheme(); //replace with your theme/createTheme
+  return (
+    //Setting Material UI locale as best practice to result in better accessibility
+    <ThemeProvider theme={createTheme(theme, hrHR)}>
+      <Example />
+    </ThemeProvider>
+  );
+};
+
+export default ExampleWithThemeProvider;
