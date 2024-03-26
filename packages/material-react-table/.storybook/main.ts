2b8914c1a8923e9945dd99b352f8013a749d697e
Kevin Van Cott <kevinvandy656@gmail.com>
2024-03-26 12:30:47 -0500

    package upgrades

--- packages/material-react-table/.storybook/main.ts
+++ packages/material-react-table/.storybook/main.ts
@@ -15,11 +15,8 @@ const config: StorybookConfig = {
     '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
-    getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@chromatic-com/storybook'),
-    getAbsolutePath('@storybook/addon-interactions'),
     getAbsolutePath('@storybook/addon-a11y'),
     getAbsolutePath('@storybook/addon-storysource'),
     getAbsolutePath('storybook-dark-mode'),
